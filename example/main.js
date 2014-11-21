0386b090ea68fe7be4e7cafb8ccf708ae7003234
Ratchagarn <ratchagarn@gmail.com>
2014-11-21 15:10:40 +0700

    Update example

--- example/main.js
+++ example/main.js
@@ -2,36 +2,57 @@
 
 'use strict';
 
+var test = document.getElementById('test');
+
 var dom = vdom('div', { className: 'dropdown' })
             .child('button', { className: 'btn btn-primary', dataToggle: 'dropdown' }, 'Dropdown')
-            .root(
+            .parent(
               vdom('ui', { className: 'dropdown-menu' })
-                .root(
+                .parent(
                   vdom('li', { className: 'active' })
                     .child('a', { href: '#' }, 'List 1')
                 )
-                .root(
+                .parent(
                   vdom('li', null)
                     .child('a', { href: '#' }, 'List 2')
                 )
-                .root(
+                .parent(
                   vdom('li', null)
                     .child('a', { href: '#' }, 'List 3')
                 )
-                .root(
+                .parent(
                   vdom('li', null)
                     .child('a', { href: '#' }, 'List 4')
                 )
             );
 
 
-document.body.appendChild(dom.el);
+document.body.appendChild(dom.node);
 
-// var $dropdown = $(dom.el);
 
-// $dropdown.on('click', function() {
-//   console.log('test click');
-// }).appendTo($('#test'));
 
+var ul = vdom('ul'),
+    lists = ['list-1', 'list-2', 'list-3', 'list-4'];
+
+lists.forEach(function(list) {
+  ul.child( vdom('li', null, list) );
+});
+
+var node = ul.parent(
+  vdom('li', null, 'parent')
+    .child('ul')
+, true);
+
+var child_ul = vdom(node);
+child_ul
+  .child('li', null, 'child-list-1')
+  .child('li', null, 'child-list-2')
+
+
+$(ul.node).find('li').on('click', function() {
+  alert(this.innerHTML);
+});
+
+document.body.appendChild(ul.node);
 
 }).call(this);
